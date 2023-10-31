86c2637a0f9da5c92ef72b66be1e360220efe016
anwar-moubakir <143129136+anwar-moubakir@users.noreply.github.com>
2023-10-31 17:30:06 -0500

    Add files via upload

---
+++ cis_121_project.py
@@ -0,0 +1,17 @@
+'''
+Anwar Moubakir
+Luke Ross
+???
+10/31/2023
+
+CIS 121 Project -- Digital Book Library
+'''
+
+#Book class with defining attributes
+class Book:
+	def __init__(self, name, author, publisher, date_published, num_pages):
+		self.name = named
+		self.author = author
+		self.publisher = publisher
+		self.date_published = date_published
+		self.num_pages = num_pages
