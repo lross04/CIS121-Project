ff1700d4b769b348424d558a778c02e88d633b39
lross04 <143128592+lross04@users.noreply.github.com>
2023-10-31 17:32:49 -0500

    Update cis_121_project.py

--- cis_121_project.py
+++ cis_121_project.py
@@ -1,7 +1,7 @@
 '''
 Anwar Moubakir
 Luke Ross
-???
+Youssef Abdelaziz
 10/31/2023
 
 CIS 121 Project -- Digital Book Library
@@ -10,7 +10,7 @@ CIS 121 Project -- Digital Book Library
 #Book class with defining attributes
 class Book:
 	def __init__(self, name, author, publisher, date_published, num_pages):
-		self.name = named
+		self.name = name
 		self.author = author
 		self.publisher = publisher
 		self.date_published = date_published
