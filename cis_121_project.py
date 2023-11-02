bb6b7337959571e9fdd8f7d0b9750034303360f5
lross04 <143128592+lross04@users.noreply.github.com>
2023-11-02 15:23:22 -0500

    Update cis_121_project.py

--- cis_121_project.py
+++ cis_121_project.py
@@ -19,33 +19,37 @@ print("quit - Quit")
 print()
 user_task = input()
 
-#Task loop
-while user_task != 'quit':
-	if user_task == 'add' or user_task == 'Add':
-		user_book_name = input("Book name: ")
-		user_book_author = input("Author: ")
-		user_book_date = input("Year published (YYYY): ")
-		user_book_genre = input("Genre: ")
-		
-		newbook = Book(user_book_name, user_book_author, user_book_date, user_book_genre)
-		
-		add_book(newbook)
+options = ('add', 'find', 'quit')
 
-	elif user_task == 'find':
-		print("How do you want to look for your book?")
-		print("name - By name")
-		print("author - By author")	
-		print("year - By year published")
-		print()
-					
-		user_method = input()
+while user_task not in options:
+
+	#Task loop
+	while user_task != 'quit':
+		if user_task == 'add' or user_task == 'Add':
+			user_book_name = input("Book name: ")
+			user_book_author = input("Author: ")
+			user_book_date = input("Year published (YYYY): ")
+			user_book_genre = input("Genre: ")
+			
+			newbook = Book(user_book_name, user_book_author, user_book_date, user_book_genre)
+			
+			add_book(newbook)
+
+		elif user_task == 'find':
+			print("How do you want to look for your book?")
+			print("name - By name")
+			print("author - By author")	
+			print("year - By year published")
+			print()
+						
+			user_method = input()
+			
+			print(find_book(user_method))
 		
-		print(find_book(user_method))
-	
-	#Return to question sequence
-	print("What do you want to do?")
-	print("add - Add a book")
-	print("find - Find a book")
-	print("quit - Quit")
-	print()
-	user_task = input()
+		#Return to question sequence
+		print("What do you want to do?")
+		print("add - Add a book")
+		print("find - Find a book")
+		print("quit - Quit")
+		print()
+		user_task = input()
